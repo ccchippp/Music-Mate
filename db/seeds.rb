d3142f6aa58d4a162e03136853774e0df1840608
ccchippp <clempke90@gmail.com>
2021-02-12 15:46:24 -0500

    artist index and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,15 @@ Song.destroy_all
 Album.destroy_all
 Artist.destroy_all
 
-
-kanye = Artist.create( name: "Kanye")
-fjm = Artist.create( name: "Father John Misty")
-zepp = Artist.create( name: "Led Zeppelin")
-nirvana = Artist.create( name: "Nirvana")
-
-lz = Album.create( title: "Led Zeppelin", release_date: 1969, artist_id: zepp.id )
-lzII = Album.create( title: "Led Zeppelin II", release_date: 1969, artist_id: zepp.id )
-lzIII = Album.create( title: "Led Zeppelin III", release_date: 1970, artist_id: zepp.id )
-lzIV = Album.create( title: "Led Zeppelin IV", release_date: 1971, artist_id: zepp.id )
-
 # Song.all.select(&:name).map(&:name) Returns song name
 # Song.all.select(&:name).map(&:artist).map(&:name) Returns artist
 
+zepp = Artist.create(name: "Led Zeppelin")
+nirvana = Artist.create(name: "Nirvana")
+kanye = Artist.create(name: "Kanye")
+fjm = Artist.create(name: "Father John Misty")
+
+lz = Album.create( title: "Led Zeppelin", release_date: 1969, artist_id: zepp.id )
 Song.create( name: "Good Times Bad Times", album_id: lz.id, artist_id: zepp.id )
 Song.create( name: "Babe I'm Gonna Leave You", album_id: lz.id, artist_id: zepp.id )
 Song.create( name: "You Shook Me", album_id: lz.id, artist_id: zepp.id )
@@ -25,3 +20,17 @@ Song.create( name: "Black Mountain Side", album_id: lz.id, artist_id: zepp.id )
 Song.create( name: "Communication Breakdown", album_id: lz.id, artist_id: zepp.id )
 Song.create( name: "I Can't Quit You Baby", album_id: lz.id, artist_id: zepp.id )
 Song.create( name: "How Many More Times", album_id: lz.id, artist_id: zepp.id )
+
+lzII = Album.create( title: "Led Zeppelin II", release_date: 1969, artist_id: zepp.id )
+Song.create( name: "Whole Lotta Love", album_id: lzII.id, artist_id: zepp.id )
+Song.create( name: "What Is and What Should Never Be", album_id: lzII.id, artist_id: zepp.id )
+Song.create( name: "The Lemon Song", album_id: lzII.id, artist_id: zepp.id )
+Song.create( name: "Thank You", album_id: lzII.id, artist_id: zepp.id )
+Song.create( name: "Heartbreaker", album_id: lzII.id, artist_id: zepp.id )
+Song.create( name: "Living Loving Maid", album_id: lzII.id, artist_id: zepp.id )
+Song.create( name: "Ramble On", album_id: lzII.id, artist_id: zepp.id )
+Song.create( name: "Moby Dick", album_id: lzII.id, artist_id: zepp.id )
+Song.create( name: "Bring It on Home", album_id: lzII.id, artist_id: zepp.id )
+
+lzIII = Album.create( title: "Led Zeppelin III", release_date: 1970, artist_id: zepp.id )
+lzIV = Album.create( title: "Led Zeppelin IV", release_date: 1971, artist_id: zepp.id )
