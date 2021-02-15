ceaf6fcf2e1f3b6cf60bd82ed0d33a83bd438cf9
ccchippp <clempke90@gmail.com>
2021-02-15 18:57:45 -0500

    still trying

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,10 +1,14 @@
 class AlbumsController < ApplicationController
-
+    before_action :find_album, only: [:show]
     def index 
         @albums = Albums.all
     end
 
     def show
+    end
+    
+    private
+    def find_album
         @album = Album.find(params[:id])
     end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,13 @@ Song.destroy_all
 Album.destroy_all
 Artist.destroy_all
 
-# Song.all.select(&:name).map(&:name) Returns song name
-# Song.all.select(&:name).map(&:artist).map(&:name) Returns artist
-
 zepp = Artist.create(name: "Led Zeppelin")
-nirvana = Artist.create(name: "Nirvana")
-kanye = Artist.create(name: "Kanye")
-fjm = Artist.create(name: "Father John Misty")
+# nirvana = Artist.create(name: "Nirvana")
+# kanye = Artist.create(name: "Kanye")
+# fjm = Artist.create(name: "Father John Misty")
 
-lz = Album.create( title: "Led Zeppelin", release_date: 1969, artist_id: zepp.id, artwork: '/app/assets/images/artwork/lz.jpg')
+lz = Album.create( title: "Led Zeppelin", release_date: 1969, artist_id: zepp.id)
+lz.artwork.attach(File.open('/app/assets/images/artwork/lz.jpg'), filename: 'lz.jpg', content_type: 'image/jpg')
 Song.create( name: "Good Times Bad Times", album_id: lz.id, artist_id: zepp.id )
 Song.create( name: "Babe I'm Gonna Leave You", album_id: lz.id, artist_id: zepp.id )
 Song.create( name: "You Shook Me", album_id: lz.id, artist_id: zepp.id )
@@ -21,19 +19,19 @@ Song.create( name: "Communication Breakdown", album_id: lz.id, artist_id: zepp.i
 Song.create( name: "I Can't Quit You Baby", album_id: lz.id, artist_id: zepp.id )
 Song.create( name: "How Many More Times", album_id: lz.id, artist_id: zepp.id )
 
-lzII = Album.create( title: "Led Zeppelin II", release_date: 1969, artist_id: zepp.id, artwork: '/app/assets/images/artwork/lzII.jpg')
-Song.create( name: "Whole Lotta Love", album_id: lzII.id, artist_id: zepp.id )
-Song.create( name: "What Is and What Should Never Be", album_id: lzII.id, artist_id: zepp.id )
-Song.create( name: "The Lemon Song", album_id: lzII.id, artist_id: zepp.id )
-Song.create( name: "Thank You", album_id: lzII.id, artist_id: zepp.id )
-Song.create( name: "Heartbreaker", album_id: lzII.id, artist_id: zepp.id )
-Song.create( name: "Living Loving Maid", album_id: lzII.id, artist_id: zepp.id )
-Song.create( name: "Ramble On", album_id: lzII.id, artist_id: zepp.id )
-Song.create( name: "Moby Dick", album_id: lzII.id, artist_id: zepp.id )
-Song.create( name: "Bring It on Home", album_id: lzII.id, artist_id: zepp.id )
+# lzII = Album.create( title: "Led Zeppelin II", release_date: 1969, artist_id: zepp.id, artwork: '/app/assets/images/artwork/lzII.jpg')
+# Song.create( name: "Whole Lotta Love", album_id: lzII.id, artist_id: zepp.id )
+# Song.create( name: "What Is and What Should Never Be", album_id: lzII.id, artist_id: zepp.id )
+# Song.create( name: "The Lemon Song", album_id: lzII.id, artist_id: zepp.id )
+# Song.create( name: "Thank You", album_id: lzII.id, artist_id: zepp.id )
+# Song.create( name: "Heartbreaker", album_id: lzII.id, artist_id: zepp.id )
+# Song.create( name: "Living Loving Maid", album_id: lzII.id, artist_id: zepp.id )
+# Song.create( name: "Ramble On", album_id: lzII.id, artist_id: zepp.id )
+# Song.create( name: "Moby Dick", album_id: lzII.id, artist_id: zepp.id )
+# Song.create( name: "Bring It on Home", album_id: lzII.id, artist_id: zepp.id )
 
-lzIII = Album.create( title: "Led Zeppelin III", release_date: 1970, artist_id: zepp.id, artwork: '/app/assets/images/artwork/lzIII.jpg' )
-lzIV = Album.create( title: "Led Zeppelin IV", release_date: 1971, artist_id: zepp.id, artwork: '/app/assets/images/artwork/lzIV.jpg' )
+# lzIII = Album.create( title: "Led Zeppelin III", release_date: 1970, artist_id: zepp.id, artwork: '/app/assets/images/artwork/lzIII.jpg' )
+# lzIV = Album.create( title: "Led Zeppelin IV", release_date: 1971, artist_id: zepp.id, artwork: '/app/assets/images/artwork/lzIV.jpg' )
 
 #File.open(File.join(Rails.root, "/app/assets/images/seed/#{file_name}.jpg"))
 # def add_artwork
